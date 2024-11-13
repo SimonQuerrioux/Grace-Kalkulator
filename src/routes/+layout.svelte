3f63755eda13a19578d5a81d398218359d35e41d
SimonQuerrioux <sim.querrioux@gmail.com>
2024-11-13 19:41:25 +0100

    kalk

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import '../app.css';
+</script>
+
+<nav class="w-full p-6 bg-orange-300">
+	<div class="flex gap-4">
+		<a href="/" class={$page.url.pathname === '/' ? 'font-bold' : ''}>Gumb</a>
+		<a href="/domov" class={$page.url.pathname === '/domov' ? 'font-bold' : ''}>Domov</a>
+		<a href="/trgovina" class={$page.url.pathname === '/trgovina' ? 'font-bold' : ''}>Trgovina</a>
+		<a href="/kalkulator" class={$page.url.pathname === '/kalkulator' ? 'font-bold' : ''}
+			>Kalkulator</a
+		>
+	</div>
+</nav>
+
+<div class="w-full h-screen bg-slate-400">
+	<slot />
+</div>
---
+++ src/routes/kalkulator/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	type Operator = '+' | '-' | '*' | '/';
+	let num1 = 0;
+	let num2 = 0;
+	let op: Operator = '+';
+	let res = 0;
+	$: {
+		if (op === '+') {
+			res = num1 + num2;
+		} else if (op === '-') {
+			res = num1 - num2;
+		} else if (op === '*') {
+			res = num1 * num2;
+		} else if (op === '/') {
+			res = num1 / num2;
+		}
+	}
+	function randomNumber() {
+		num1 = Math.floor(Math.random() * 100);
+		num2 = Math.floor(Math.random() * 100);
+
+		const symbols: Operator[] = ['+', '-', '*', '/'];
+		const idx = Math.floor(Math.random() * 4);
+
+		op = symbols[idx];
+	}
+</script>
+
+<h1 class="text-4xl">KALKULATOR</h1>
+<input bind:value={num1} type="number" placeholder="0" />
+<select bind:value={op}>
+	<option value="+">+, plus</option>
+	<option value="-">-, minus</option>
+	<option value="*">*, krat</option>
+	<option value="/">/, deljeno</option>
+</select>
+<input bind:value={num2} type="number" placeholder="0" />
+<button class="p-3 bg-slate-600 text-white rounded-2xl hover:bg-slate-700" on:click={randomNumber}
+	>RANDOM</button
+>
+<p>{num1} {op} {num2} = {res}</p>
